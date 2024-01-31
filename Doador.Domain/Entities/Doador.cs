062a7c4fcf5ec781ca77824ec97285ec1e666014
deverton1 <evertonrabelo113@gmail.com>
2024-01-30 22:03:14 -0300

    entidades adicionadas

---
+++ Doador.Domain/Entities/Doador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doador.Domain.Entities
+{
+    public class Doador
+    {
+        public int DoadorId { get; set; }
+        public string Nome { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public int CEP { get; set; }
+        public string Email { get; set; }
+        public int Telefone { get; set; }
+    }
+}
---
+++ Doador.Domain/Entities/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doador.Domain.Entities
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string NomeProd { get; set; }
+        public string DescriProd { get; set; }
+        public Enum ECategoria { get; set; }
+        public bool Disponivel { get; set; }
+        public int DoadorId { get; set; } //chave estrangeira
+    }
+}
